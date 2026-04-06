f956a17e118aaf779acafd3f1bd7d3e773ff9e5b
retro98boy <retro98boy@qq.com>
2026-04-06 18:06:15 +0800

    extensions: amlogic-fip-blobs: Update repo URL

--- extensions/amlogic-fip-blobs.sh
+++ extensions/amlogic-fip-blobs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 function fetch_sources_tools__amlogic-fip-blobs() {
-	fetch_from_repo "https://github.com/retro98boy/amlogic-fip-blobs" "amlogic-fip-blobs" "commit:774cabf4275676b01af6d644f3de58cd2f7e8c10"
+	fetch_from_repo "https://github.com/retro98boy/amlogic-fip-blobs" "amlogic-fip-blobs" "commit:44326a3748cc4d45b7cd0546063b7d3a2e6b3eb1"
 }
